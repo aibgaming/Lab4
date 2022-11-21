4748788b79695eb2c11b561471decfbf5b5971fc
Autri Ilesh Banerjee <69169584+aibgaming@users.noreply.github.com>
2022-11-20 20:22:19 -0500

    Update Lab4.cpp

--- Lab4/Lab4.cpp
+++ Lab4/Lab4.cpp
@@ -111,90 +111,94 @@ void quickSort(int a[], int p, int r)
 }
  
  
-int getMax(int array[], int size) {
-   int max = array[1];
-   for(int i = 2; i<=size; i++) {
-      if(array[i] > max)
-         max = array[i];
-   }
-   return max; //the max element from the array
-}
-void countSort(int *array, int size) {
-   int output[size+1];
-   int max = getMax(array, size);
-   int count[max+1];     //create count array (max+1 number of elements)
-   for(int i = 0; i<=max; i++)
-      count[i] = 0;     //initialize count array to all zero
-   for(int i = 1; i <=size; i++)
-      count[array[i]]++;     //increase number count in count array.
-   for(int i = 1; i<=max; i++)
-      count[i] += count[i-1];     //find cumulative frequency
-   for(int i = size; i>=1; i--) {
-      output[count[array[i]]] = array[i];
-      count[array[i]] -= 1; //decrease count for same numbers
-   }
-   for(int i = 1; i<=size; i++) {
-      array[i] = output[i]; //store output array to main array
-   }
-}
-
-
-void countSortRadix(int arr[], int n, int exp)
+//Function to get the largest element from an array
+int getMax(int array[], int n)
 {
-    int output[n]; 
-    int i, count[10] = { 0 };
- 
-    for (i = 0; i < n; i++)
-        count[(arr[i] / exp) % 10]++;
-
-    for (i = 1; i < 10; i++)
-        count[i] += count[i - 1];
-    
-    for (i = n - 1; i >= 0; i--) {
-        output[count[(arr[i] / exp) % 10] - 1] = arr[i];
-        count[(arr[i] / exp) % 10]--;
-    }
-
-    for (i = 0; i < n; i++)
-        arr[i] = output[i];
+  int max = array[0];
+  for (int i = 1; i < n; i++) if (array[i] > max)
+      max = array[i];
+  return max;
 }
- 
 
-void radixSort(int arr[], int n)
+//Using counting sort to sort the elements in the basis of significant places
+void countSort(int array[], int size, int place) 
 {
-    int m = getMax(arr, n);
-    for (int exp = 1; m / exp > 0; exp *= 10)
-        countSortRadix(arr, n, exp);
+  const int max = 10;
+  int output[size];
+  int count[max];
+
+  for (int i = 0; i < max; ++i)
+    count[i] = 0;
+
+  //Calculate count of elements
+  for (int i = 0; i < size; i++)
+    count[(array[i] / place) % 10]++;
+
+  //Calculating cumulative count
+  for (int i = 1; i < max; i++)
+    count[i] += count[i - 1];
+
+  //Placing the elements in sorted order
+  for (int i = size - 1; i >= 0; i--) 
+  {
+    output[count[(array[i] / place) % 10] - 1] = array[i];
+    count[(array[i] / place) % 10]--;
+  }
+
+  for (int i = 0; i < size; i++)
+    array[i] = output[i];
+}
+
+//Main function to implement radix sort
+void radixSort(int array[], int size) 
+{
+  //Getting maximum element
+  int max = getMax(array, size);
+
+  //Applying counting sort to sort elements based on place value.
+  for (int place = 1; max / place > 0; place *= 10)
+    countSort(array, size, place);
 }
  
 
 
 void heapify(int arr[], int n, int i)
 {
-    int largest = i; 
-    int l = 2 * i + 1; 
-    int r = 2 * i + 2; 
+    int largest = i;
+    int l = 2*i + 1;
+    int r = 2*i + 2;
  
-    if (l < n && arr[l] > arr[largest]){}
+    //If left child is larger than root
+    if (l < n && arr[l] > arr[largest])
         largest = l;
  
+    //If right child largest 
     if (r < n && arr[r] > arr[largest])
         largest = r;
  
-    if (largest != i) {
+    //If root is nor largest
+    if (largest != i)
+    {
         swap(arr[i], arr[largest]);
  
+        //Recursively heapifying the sub-tree
         heapify(arr, n, largest);
     }
 }
- 
+
 void heapSort(int arr[], int n)
 {
+    
     for (int i = n / 2 - 1; i >= 0; i--)
         heapify(arr, n, i);
  
-    for (int i = n - 1; i >= 0; i--) {
+    //One by one extract an element from heap
+    for (int i=n-1; i>=0; i--)
+    {
+        //Moving current root to end
         swap(arr[0], arr[i]);
+ 
+        //Calling max heapify on the reduced heap
         heapify(arr, i, 0);
     }
 }
@@ -204,8 +208,6 @@ int randomGen(int low, int high)
 {
     return low + rand() % (high - low + 1);
 }
-
-
 
  #include <chrono>
 int main()
@@ -225,24 +227,31 @@ int main()
     cout<< endl;
     
     int arrlength = sizeof(myarr)/sizeof(myarr[1]);
+    int (*pMyarr) = myarr;
+    
+    typedef std::chrono::high_resolution_clock Clock;
+    auto t1 = Clock::now();
+    //Doing the sorts
+    
     //bubbleSort(myarr, arrlength);
     //insertionSort(myarr, arrlength);
-    int (*pMyarr) = myarr;
     //mergeSort(pMyarr, 0, n-1);
     //quickSort(myarr, 0, n-1);
-    countSort(pMyarr, n-1);
-    //radixSort(myarr, n-1);
+    //countSort(pMyarr, n-1);
+    radixSort(myarr, n);
+    //heapSort(myarr,n);
     
     //Printing array after sorting
+
+
+
+    auto t2 = Clock::now();
+    cout << "Delta t2-t1: "  <<chrono::duration_cast<chrono::nanoseconds>(t2 - t1).count() << " nanoseconds" << std::endl;
+    
     for (int i = 0; i < n; i++) {
         cout << myarr[i] << " ";
     }
     cout<< endl;
-
-    typedef std::chrono::high_resolution_clock Clock;
-    auto t1 = Clock::now();
-    auto t2 = Clock::now();
-    cout << "Delta t2-t1: "  <<chrono::duration_cast<chrono::nanoseconds>(t2 - t1).count() << " nanoseconds" << std::endl;
 
 
     cout << "Hello World! Lab 4 is here!\n";
